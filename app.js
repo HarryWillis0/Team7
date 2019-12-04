595f087a7e0f3b21a5e0e53f314e8b2ecf239479
Allblue0127 <50658585+Allblue0127@users.noreply.github.com>
2019-12-04 09:53:36 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,177 +1,51 @@
-/**
- * app.js
- * handle client requests and serve appropiate pages
- * 
- * Author: Harry Willis
- */
 const express = require("express");
+const mysql= require ('mysql');
 const app = express();
-const path = require("path");
-const session = require("express-session");
-const inCust = require("./public/scripts/insert_cust");
-const conn = require("./public/scripts/DBConnect");
-const verify = require("./public/scripts/login_validate");
-const getCustId = require("./public/scripts/get_cust_id");
-const inBk = require("./public/scripts/insert_booking");
 
-/* init session */
-app.use(session({
-    secret: 'topsecret',
-    saveUninitialized: true,
-    resave: true
-}));
-var sess;
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'Joe',
+    password: 'password',
+    database: 'travelexperts'
+});
 
-/* serve css/js/image files */
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+});
+
+
+app.use(express.static("views", {"extensions":["html", "htm"]}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-/* parse incoming requests */
-app.use(express.urlencoded({extended: true}));
 
-/* tell express where to look for pug files */
-app.set("views", path.join(__dirname, "views"));
-
-/* tell express we are using pug template engine */
-app.set("view engine", "pug");
-
-/* serve home page */
 app.get("/", (req, res) => {
-    sess = req.session;
-    
-    if (sess.firstName) {
-        res.render("index", {logOut: "Logout", bookIfIn: "Book Now!"});
-    } else {
-        res.render("index", {logIn: "Login"});
-    }
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-/* serve contact page */
-app.get("/contact", (req, res) => {
-    sess = req.session;
-
-    res.render(__dirname + "/views/contact");
-});
-
-/* serve register page */
 app.get("/register", (req, res) => {
-    sess = req.session;
-
-    res.render(__dirname + "/views/register");
+    res.sendFile(__dirname + "/views/register.html");
 });
 
-/* serve post action from form on register.html */
-app.post("/sendData", (req, res) => {
-    sess = req.session;
-
-    /* insert new customer into database */
-    inCust.insertCust(req.body, conn);
-    
-    /* log them in */
-    sess.firstName = req.body.firstname;
-    sess.lastName = req.body.lastname;
-    sess.email = req.body.email;
-    
-    /* serve a personalized thank you page */
-    res.render('thanks', {greeting: "for registering with us " + sess.firstName + "!", logOut: "Logout"});
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/views/contact.html");
 });
 
-/* serve vacation page */
-app.get("/vacation", (req, res) => {
-    sess = req.session;
-
-    res.render(__dirname + "/views/vacation.pug");
+app.get("/get-agency", function (req, res) {
+    conn.query("SELECT AgencyId, AgncyAddress,AgncyPhone FROM agencies", function (err, result) {
+        if (err) throw err;
+        // console.log(result)
+        res.send(result)
+    })
 });
-
-/* serve login page */
-app.get("/login", (req, res) => {
-    sess = req.session;
-
-    res.render(__dirname + "/views/login.pug");
-});
-
-/* serve login post */
-app.post("/login", (req, result) => {
-    sess = req.session;
-
-    verify.verify(req.body, conn, (success) => {
-        /* act on callback */
-        if (success) {
-            /* login successful */
-            
-            /* get their name from db to update session for some personalization */
-            var getName = "SELECT `CustFirstName`, `CustLastName` FROM `customers` WHERE CustEmail = ?";
-            
-            conn.query(getName, [req.body.userName], (err, res, fields) => {
-                /* this has to have a result if login was successful */
-
-                /* set some session properties */
-                sess.firstName = res[0].CustFirstName;
-                sess.lastName = res[0].CustLastName;
-                sess.email = req.body.userName;
-                
-                /* username and password matched, send home */
-                result.render("index", {logOut: "Logout", bookIfIn: "Book now!"});
-            });
-        } else {
-            result.render("login.pug", {errLogin: "Sorry we couldn't find you..."});
-        }
-    });
-});
-
-/* serve bookings page */
-app.get("/bookings", (req, res) => {
-    sess = req.session;
-    
-    /* serve different options on page depending if user logged in */
-    if (sess.firstName) {
-        res.render(__dirname + "/views/bookings.pug", {loggedIn: "Book a trip now!"});
-    } else {
-        res.render("login", {needInToBook: "Sorry you must login before booking."});
-    }
-});
-
-/* serve bookings post */
-app.post("/book", (req, res) => {    
-    sess = req.session;
-    
-    /* get customerID */
-    getCustId.findId(sess.firstName, sess.lastName, sess.email, conn, (id) => {
-        /* act on callback */
-        if (id) {
-            /* create booking */
-            inBk.insertBook(req.body, id, conn);
-            
-            /* serve personalized thank you page */
-            res.render("thanks.pug", {greeting: "for taking an adventure with us " + sess.firstName + "!", logOut: "Logout"});
-        } else {
-            console.log("couldn't find user to make booking");
-
-            res.render("bookings.pug", {errFind: "Sorry we couldn't find you in our system. You may need to register first."});
-        }
-    });
-});
-
-/* serve logout */
-app.use("/logout", (req, res) => {
-    /* save customer name */
-    var currFName = sess.firstName;
-
-    /* close session and send customer to personalized thanks page */
-    req.session.destroy((err) => {
-        if(err) throw err;
-
-        /* serve personalized thank you page */
-        res.render("thanks.pug", {greeting: "for visiting " + currFName + ". Hope to see you soon!"});
+app.get("/get-agents", function (req, res) {
+    conn.query("SELECT * FROM agents", function (err, result) {
+        if (err) throw err;
+        res.send(result)
     })
 });
 
-/* serve any other requests to 404 page */
-app.use((req, res) => {
-    res.render(__dirname + "/views/404");
-});
-
-/* set up server on port 8000 */
-app.listen(8000, (err) => {
-    if (err) throw err;
-    console.log("Listening on port 8000...");
+app.listen(3000, () => {
+    console.log("Listening on port 3000. Server started.");
 });
